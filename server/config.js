aeab4e3243a20ad239407812970fe81bb79238cf
sangpham254 <121900059+sangpham254@users.noreply.github.com>
2025-05-19 07:39:54 -0400

    Add files via upload

---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB_URI: process.env.MONGODB_URI,
+  GOOGLE_BOOKS_API_KEY: process.env.GOOGLE_BOOKS_API_KEY,
+  PORT: process.env.PORT || 3000,
+  NODE_ENV: process.env.NODE_ENV || 'production'
+};
